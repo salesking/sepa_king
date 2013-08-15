fd898f7d23d9c1eb7dd48b786ba8b0b5e747f614
Georg Ledermann <mail@georg-ledermann.de>
2013-08-15 08:49:37 +0200

    Moved xsd files to lib/schema

--- spec/support/custom_matcher.rb
+++ spec/support/custom_matcher.rb
@@ -2,7 +2,7 @@ require 'rspec/expectations'
 
 RSpec::Matchers.define :validate_against do |xsd|
   match do |actual|
-    schema_doc = XML::Document.file("spec/fixtures/#{xsd}")
+    schema_doc = XML::Document.file("lib/schema/#{xsd}")
     schema = XML::Schema.document(schema_doc)
     actual.validate_schema(schema)
   end
