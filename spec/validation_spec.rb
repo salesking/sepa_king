dc8c29b397bfe262124d892e356002fa4ef6aeb2
Georg Ledermann <mail@georg-ledermann.de>
2013-08-23 20:27:25 +0200

    Spec folder cleanup

---
+++ spec/validation_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'Credit Transfer Initiation' do
+  it "should validate example file" do
+    XML::Document.file('spec/examples/pain.001.002.03.xml').should validate_against('pain.001.002.03.xsd')
+  end
+end
+
+describe 'Direct Debit Initiation' do
+  it 'should validate example file' do
+    XML::Document.file('spec/examples/pain.008.002.02.xml').should validate_against('pain.008.002.02.xsd')
+  end
+end
