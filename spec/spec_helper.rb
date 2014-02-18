5849698ef348001640f80bb53fb0fed410806f90
Kashif Rasul <kashif.rasul@gmail.com>
2014-02-18 16:59:37 +0100

    fixed rspec warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@
 
 require 'simplecov'
 require 'coveralls'
+require 'rspec/collection_matchers'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
@@ -26,9 +27,11 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].e
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
