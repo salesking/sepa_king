ba5a836cb9d17beed2ad1d7836220e2615f99745
Georg Ledermann <mail@georg-ledermann.de>
2014-02-19 16:08:11 +0100

    Factories: Housekeeping

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
-def credit_transfer_transaction
+def credit_transfer_transaction(attributes={})
   { name:                   'Telekomiker AG',
     bic:                    'PBNKDEFF370',
     iban:                   'DE37112589611964645802',
     amount:                 102.50,
     reference:              'XYZ-1234/123',
     remittance_information: 'Rechnung vom 22.08.2013'
-  }
+  }.merge(attributes)
 end
 
 def direct_debt_transaction(attributes={})
@@ -15,10 +15,10 @@ def direct_debt_transaction(attributes={})
     bic:                       'GENODEF1JEV',
     iban:                      'DE68210501700012345678',
     amount:                    750.00,
-    reference:                 attributes[:reference] || 'XYZ/2013-08-ABO/6789',
+    reference:                 'XYZ/2013-08-ABO/6789',
     remittance_information:    'Vielen Dank für Ihren Einkauf!',
     mandate_id:                'K-08-2010-42123',
     mandate_date_of_signature: Date.new(2010,7,25),
-    requested_date:            attributes[:requested_date]
-  }
+    requested_date:            Date.today + 1
+  }.merge(attributes)
 end
