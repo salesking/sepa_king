0c38a07fb029c9116bf6a223ee3c7be169e721f8
Oriol Francès <oriolfa@gmail.com>
2016-03-18 17:36:41 +0100

    Spain says that the BIC is not required for pain.001.001.03... standards be crazy, man

--- lib/sepa_king/transaction/credit_transfer_transaction.rb
+++ lib/sepa_king/transaction/credit_transfer_transaction.rb
@@ -15,7 +15,7 @@ module SEPA
     def schema_compatible?(schema_name)
       case schema_name
       when PAIN_001_001_03, PAIN_001_002_03
-        self.bic.present? && self.service_level == 'SEPA'
+        self.service_level == 'SEPA'
       when PAIN_001_003_03
         true
       end
