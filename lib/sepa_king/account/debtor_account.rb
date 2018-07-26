c0adddd0a91a93f59696570830a88d26dfffffb1
Vladimir Pavlyukov <vladimir@playpass.be>
2018-07-26 08:44:32 +0200

    Add creditor_identifier to debtor account, adding external_organisation_id_code to account

--- lib/sepa_king/account/debtor_account.rb
+++ lib/sepa_king/account/debtor_account.rb
@@ -1,5 +1,10 @@
 # encoding: utf-8
 module SEPA
   class DebtorAccount < Account
+    attr_accessor :creditor_identifier
+
+    validates_with CreditorIdentifierValidator,
+                   message: "%{value} is invalid",
+                   if: ->(account) { account.creditor_identifier.present? }
   end
 end
