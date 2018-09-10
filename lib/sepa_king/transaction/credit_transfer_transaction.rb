f7287e83146b8a94ae3b08d2bee6ec683452aa35
Reinier de Lange <rjdelange@icloud.com>
2018-09-10 16:12:18 +0200

    Make service level optional for pain.001.001.03

--- lib/sepa_king/transaction/credit_transfer_transaction.rb
+++ lib/sepa_king/transaction/credit_transfer_transaction.rb
@@ -3,19 +3,19 @@ module SEPA
   class CreditTransferTransaction < Transaction
     attr_accessor :service_level
 
-    validates_inclusion_of :service_level, :in => %w(SEPA URGP)
+    validates_inclusion_of :service_level, :in => %w(SEPA URGP), :allow_nil => true
 
     validate { |t| t.validate_requested_date_after(Date.today) }
 
     def initialize(attributes = {})
       super
-      self.service_level ||= 'SEPA'
+      self.service_level ||= 'SEPA' if self.currency == 'EUR'
     end
 
     def schema_compatible?(schema_name)
       case schema_name
       when PAIN_001_001_03
-        self.service_level == 'SEPA'
+        !self.service_level || (self.service_level == 'SEPA' && self.currency == 'EUR')
       when PAIN_001_002_03
         self.bic.present? && self.service_level == 'SEPA' && self.currency == 'EUR'
       when PAIN_001_003_03
